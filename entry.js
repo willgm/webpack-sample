27b9022e61bca208d151261b956f033feb2fb391
William Grasel <william.grasel@scup.com>
2015-08-03 23:07:46 +0530

    mixin didifferent types of modularization

--- entry.js
+++ entry.js
@@ -1,6 +1,10 @@
 var _ = require("lodash");
 var msg = require("./message.js");
+var amd = require("./message-amd.js");
+
+var Backbone = require("backbone");
 
 require("./style.css");
 
 console.log(_.capitalize(msg));
+console.log(_.capitalize(amd));
